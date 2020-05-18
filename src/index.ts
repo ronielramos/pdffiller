dbe813313d59e771a7c6e9319a587a0c9720fd9c
Roniel <roniel_ramos@hotmail.com>
2020-05-17 22:33:06 -0300

    refactor: remove older methods to init the refactor and tests

--- src/index.ts
+++ src/index.ts
@@ -8,130 +8,38 @@
 *                PDF file with the form fields populated.
 */
 
-import child_process from 'child_process'
-import fs from 'fs'
-import _ from 'lodash'
+import { exec } from 'child_process'
 
-class pdffiller {
-  constructor (private execFile = child_process.execFile) {
+// generate_fdf
+// fill_form < FDF data filename | XFDF data filename | - | PROMPT >
+// pdftk form.pdf fill_form data.fdf output form.filled.pdf
+
+class Pdffiller {
+  constructor (private execProcess = exec) {
     console.log('init')
   }
 
-  public mapForm2PDF (formFields: { [key: string]: string } | { [key: string]: string }[], convMap: { [key: string]: unknown }) {
-    const tmpFDFData = this.convFieldJson2FDF(formFields)
-    const tmpFDFData2 = _.mapKeys(tmpFDFData, function (value: unknown, key: string) {
-      try {
-        convMap[key]
-      } catch (err) {
-        return key
-      }
-      return convMap[key]
-    })
+  public convFieldJson2FDF (fieldJson: { [key: string]: unknown }) {
+    const jsonObj: { [key: string]: string | number } = {}
 
-    return tmpFDFData2
-  }
-
-  public convFieldJson2FDF (fieldJson: { [key: string]: string } | { [key: string]: string }[]) {
-    const _keys = _.map(fieldJson, 'title')
-    const _values = _.map(fieldJson, 'fieldValue')
-
-    const _values2 = _.map(_values, function (val: boolean) {
-      if (val === true) {
-        return 'Yes'
-      } else if (val === false) {
-        return 'Off'
-      }
-      return val
-    })
-
-    const jsonObj = _.zipObject(_keys, _values2)
+    Object
+      .entries(fieldJson)
+      .map(([key, value]) => {
+        if (value == null) return [key, '']
+        if (value === true) return [key, 'Yes']
+        if (value === false) return [key, 'Off']
+        if (typeof value === 'number' || typeof value === 'string') return [key, value]
+        throw new Error(`Value ${value} of type ${typeof value} is invalid`)
+      })
+      .forEach(([key, value]) => {
+        jsonObj[key] = value
+      })
 
     return jsonObj
   }
 
-  public generateFieldJson (sourceFile: string, nameRegex: string | null, callback: Function) {
-    const regName: string | RegExp = (nameRegex !== null && (typeof nameRegex) === 'object')
-      ? nameRegex
-      : /FieldName: ([^\n]*)/
-
-    const regType = /FieldType: ([A-Za-z\t .]+)/
-    const regFlags = /FieldFlags: ([0-9\t .]+)/
-    const fieldArray: { [key: string]: string }[] = []
-
-    this.execFile('pdftk', [sourceFile, 'dump_data_fields_utf8'], function (error: Error, stdout: object, stderr: object) {
-      if (error) {
-        console.log('exec error: ' + error)
-        return callback(error, null)
-      }
-
-      const fields = stdout.toString().split('---').slice(1)
-      fields.forEach(function (field: string) {
-        const currField: { [key: string]: string } = {}
-
-        currField.title = (field.match(regName) || '')[1]?.trim() || ''
-
-        if (field.match(regType)) currField.fieldType = (field.match(regType) || '')[1]?.trim() || ''
-        else currField.fieldType = ''
-
-        if (field.match(regFlags)) currField.fieldFlags = (field.match(regFlags) || '')[1]?.trim() || ''
-        else currField.fieldFlags = ''
-
-        currField.fieldValue = ''
-
-        fieldArray.push(currField)
-      })
-
-      return callback(null, fieldArray)
-    })
-  }
-
-  public generateFDFTemplate (sourceFile: string, nameRegex: string | null, callback: Function) {
-    this.generateFieldJson(sourceFile, nameRegex, function (err: Error, _form_fields: object) {
-      if (err) {
-        console.log('exec error: ' + err)
-        return callback(err, null)
-      }
-
-      return callback(null, this.convFieldJson2FDF(_form_fields))
-    })
-  }
-
-  public fillFormWithOptions (sourceFile: string, destinationFile: string, fieldValues: object, shouldFlatten: boolean, tempFDFPath: string | undefined, callback: Function) {
-    // Generate the data from the field values.
-    var randomSequence = Math.random().toString(36).substring(7)
-    var currentTime = new Date().getTime()
-    var tempFDFFile = 'temp_data' + currentTime + randomSequence + '.fdf'
-    var tempFDF = (typeof tempFDFPath !== 'undefined' ? tempFDFPath + '/' + tempFDFFile : tempFDFFile)
-
-    var formData = fdf.generator(fieldValues, tempFDF)
-
-    var args = [sourceFile, 'fill_form', tempFDF, 'output', destinationFile]
-    if (shouldFlatten) {
-      args.push('flatten')
-    }
-    this.execFile('pdftk', args, function (error: Error, stdout: object, stderr: object) {
-      if (error) {
-        console.log('exec error: ' + error)
-        return callback(error)
-      }
-      // Delete the temporary fdf file.
-      fs.unlink(tempFDF, function (err: Error) {
-        if (err) {
-          return callback(err)
-        }
-        // console.log( 'Sucessfully deleted temp file ' + tempFDF );
-        return callback()
-      })
-    })
-  }
-
-  public fillFormWithFlatten (sourceFile: string, destinationFile: string, fieldValues: object, shouldFlatten: boolean, callback: Function) {
-    this.fillFormWithOptions(sourceFile, destinationFile, fieldValues, shouldFlatten, undefined, callback)
-  }
-
   public fillForm (sourceFile: string, destinationFile: string, fieldValues: object, callback: Function) {
-    this.fillFormWithFlatten(sourceFile, destinationFile, fieldValues, true, callback)
   }
 }
 
-export default new pdffiller()
+export default new Pdffiller()
